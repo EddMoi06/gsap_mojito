0967f7f5631296fde051f699e0d693a998d059ca
Moises Mejia <eddymm06@gmail.com>
2025-06-25 21:56:44 -0700

    new file:   constants/index.ts 	modified:   index.html 	new file:   public/fonts/Modern Negra Demo.ttf 	new file:   public/images/abt1.png 	new file:   public/images/abt2.png 	new file:   public/images/abt3.png 	new file:   public/images/abt4.png 	new file:   public/images/abt5.png 	new file:   public/images/arrow.png 	new file:   public/images/check.png 	new file:   public/images/cocktail-left-leaf.png 	new file:   public/images/cocktail-right-leaf.png 	new file:   public/images/cup-2.png 	new file:   public/images/drink1.png 	new file:   public/images/drink2.png 	new file:   public/images/drink3.png 	new file:   public/images/drink4.png 	new file:   public/images/fav.png 	new file:   public/images/fb.png 	new file:   public/images/footer-drinks.png 	new file:   public/images/footer-left-leaf.png 	new file:   public/images/footer-right-leaf.png 	new file:   public/images/hero-left-leaf.png 	new file:   public/images/hero-right-leaf.png 	new file:   public/images/insta.png 	new file:   public/images/left-arrow.png 	new file:   public/images/logo.png 	new file:   public/images/mask-img.png 	new file:   public/images/noise.png 	new file:   public/images/profile1.png 	new file:   public/images/profile2.png 	new file:   public/images/profile3.png 	new file:   public/images/profile4.png 	new file:   public/images/right-arrow.png 	new file:   public/images/slider-left-leaf.png 	new file:   public/images/slider-right-leaf.png 	new file:   public/images/under-img.jpg 	new file:   public/images/x.png 	new file:   public/readme/hero.png 	new file:   public/readme/jsmpro.png 	new file:   public/readme/thumbnail.png 	new file:   public/readme/videokit.png 	new file:   public/videos/input.mp4 	new file:   public/videos/output.mp4 	modified:   src/App.tsx 	new file:   src/components/About.tsx 	new file:   src/components/Art.tsx 	new file:   src/components/Cocktails.tsx 	new file:   src/components/Contact.tsx 	new file:   src/components/Hero.tsx 	new file:   src/components/Menu.tsx 	new file:   src/components/Navbar.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import gsap from "gsap"
 import { ScrollTrigger } from "gsap/all"
 import _SplitText from "gsap/SplitText"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Cocktails from "./components/Cocktails"
+import About from "./components/About"
+import Art from "./components/Art"
+import Menu from "./components/Menu"
+import Contact from "./components/Contact"
 
 gsap.registerPlugin(ScrollTrigger, _SplitText)
 
 const App = () => {
   return (
-    <div className="flex-center h-[100vh]">
-      <h1 className="text-3xl text-indigo-300">Hello, GSAP!</h1>
-    </div>
+    <main>
+      <Navbar/>
+      <Hero/>
+      <Cocktails/>
+      <About/>
+      <Art/>
+      <Menu/>
+      <Contact/>
+    </main>
   )
 }
 
